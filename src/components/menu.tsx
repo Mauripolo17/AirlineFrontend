40af5acb6df4bcda441d69e226b82e9b3eed14c6
Mauricio Polo <mauriciopoloem@gmail.com>
2024-11-26 10:36:16 -0500

    Se agregaron filtros para buscar viajes y formulario para reservar

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -2,42 +2,46 @@
 import Viajes from "./viajes.tsx";
 import { useEffect, useState } from "react";
 import { Vuelo, vueloService } from "../api/vuelosService.tsx";
-import BuscadorViajes  from "./buscadorViajes.tsx";
+import BuscadorViajes from "./buscadorViajes.tsx";
 import CarouselComponent from "./Carousel.tsx";
 import { ResultadosDrBusqueda } from "./ResultadosDrBusqueda.tsx";
-
+import { Reserva } from "./reserva.tsx";
+import { ReservaProvider, useReservaContext } from "../context/ReservaContext.tsx";
 
 function Menu() {
-
   const [vuelos, setVuelos] = useState<Vuelo[]>([]);
   const [vuelos2, setVuelos2] = useState<Vuelo[]>([]);
-  const [searchMode, setSearchMode] = useState<boolean>(false);
-  
+  const {searchMode, setSearchMode} = useReservaContext();
 
   useEffect(() => {
     loadVuelos();
-    setVuelos2(vuelos.slice(0,5));
-    console.log(vuelos2);
+    setVuelos2(vuelos.slice(0, 5));
+    // console.log(vuelos2);
   }, [searchMode]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const loadVuelos = async () => {
     try {
       const getVuelos = await vueloService.getVuelos();
+      const shuffledVuelos = getVuelos.sort(() => Math.random() - 0.5);
       setVuelos(getVuelos);
-      setVuelos2(getVuelos.slice(0,5));
+      setVuelos2(shuffledVuelos.slice(0, 5));
     } catch (error) {
       console.log(error);
     }
   };
 
-
   return (
     <div>
-     <BuscadorViajes setSearchMode={setSearchMode} />
-      {!searchMode ? <><CarouselComponent />
-        <Viajes vuelos={vuelos2} /></> : <ResultadosDrBusqueda/>}
-      
-    </div>
+        <BuscadorViajes />
+        {!searchMode ? (
+          <>
+            <CarouselComponent />
+            <Viajes vuelos={vuelos2} />
+          </>
+        ) : (
+          <ResultadosDrBusqueda />
+        )}
+      </div>
   );
 }
 
